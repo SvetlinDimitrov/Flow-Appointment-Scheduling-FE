a217ad226bb0fbd1761afe03d728ec040dac8b6f
Светлин Димитров <dimitrovmail99@gmail.com>
2024-09-19 17:33:29 +0300

    Improve the UI of the footer. Now, when the screen becomes too small, an icon appears that can be clicked to display all the paths.

--- src/shared/core/footer/Footer.tsx
+++ src/shared/core/footer/Footer.tsx
@@ -1,31 +1,34 @@
-import {AppBar, Box, Toolbar, Typography} from '@mui/material';
+import {AppBar, Box, Toolbar, Typography, useMediaQuery, useTheme} from '@mui/material';
 import {Link} from 'react-router-dom';
 import {paths} from "../../paths/paths.ts";
+import FooterMenu from './FooterMenu';
 
 const Footer = () => {
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
+
   return (
-    <AppBar position={'static'} sx={{
-      top: 'auto',
-      bottom: 0,
-    }}>
-      <Toolbar sx={{
-        display: 'flex',
-        justifyContent: 'center',
-        gap: 2
-      }}>
+    <AppBar position={'static'} sx={{top: 'auto', bottom: 0}}>
+      <Toolbar sx={{display: 'flex', justifyContent: 'center', gap: 2}}>
         <Typography variant={"subtitle1"} color={"inherit"}>
           © 2024 Prime Holding
         </Typography>
-        <Box display={'flex'} gap={2}>
-          {paths.defaultPaths.map((path, index) => (
-            <Link key={index} to={Object.values(path)[0]}
-                  style={{textDecoration: 'none', color: 'inherit'}}>
-              <Typography variant={"subtitle1"} color={"inherit"}>
-                {Object.keys(path)[0]}
-              </Typography>
-            </Link>
-          ))}
-        </Box>
+        {isSmallScreen ? (
+          <FooterMenu/>
+        ) : (
+          <Box display={'flex'} gap={2}>
+            {paths.defaultPaths.map((path, index) => (
+              <Link key={index}
+                    to={Object.values(path)[0]}
+                    style={{textDecoration: 'none', color: 'inherit'}}
+              >
+                <Typography variant={"subtitle1"} color={"inherit"}>
+                  {Object.keys(path)[0]}
+                </Typography>
+              </Link>
+            ))}
+          </Box>
+        )}
       </Toolbar>
     </AppBar>
   );
